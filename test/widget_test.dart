473d7aa56f89b5218688cd00685b3e74a8931f3c
Silverio MG <atopecode@gmail.com>
2019-11-23 18:15:53 +0100

    Change app.dart directory

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 
-import 'package:flutter_listitems/src/app/app/app.dart';
+import 'package:flutter_listitems/src/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 
